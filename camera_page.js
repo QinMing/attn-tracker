c36f5c4f1657cf033d2ef5622817cbaf61af5341
Ming Qin <ming.qin@hulu.com>
2019-10-05 11:47:54 -0700

    camera working

--- camera_page.js
+++ camera_page.js
@@ -1,40 +1,55 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {Camera, Permissions} from 'expo';
-
-import styles from './styles';
+import {Text, View, TouchableOpacity} from 'react-native';
+import * as Permissions from 'expo-permissions';
+import {Camera} from 'expo-camera';
 
 export default class CameraPage extends React.Component {
-  camera = null;
-
   state = {
     hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
   };
 
   async componentDidMount() {
-    const camera = await Permissions.askAsync(Permissions.CAMERA);
-    const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-    const hasCameraPermission = (camera.status === 'granted' && audio.status === 'granted');
-
-    this.setState({hasCameraPermission});
-  };
+    const {status} = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission: status === 'granted'});
+  }
 
   render() {
     const {hasCameraPermission} = this.state;
-
     if (hasCameraPermission === null) {
       return <View/>;
     } else if (hasCameraPermission === false) {
-      return <Text>Access to camera has been denied.</Text>;
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{flex: 1}}>
+          <Camera style={{flex: 1}} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text style={{fontSize: 18, marginBottom: 10, color: 'white'}}> Flip </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
     }
-
-    return (
-      <View>
-        <Camera
-          style={styles.preview}
-          ref={camera => this.camera = camera}
-        />
-      </View>
-    );
-  };
-};
+  }
+}
